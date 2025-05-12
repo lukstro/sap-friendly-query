72ec748632c77cca59d78101def9bb98260b3b6e
Lucas Castro <lukstro@gmail.com>
2025-05-11 22:54:19 -0300

    Create table_mappings.py

---
+++ sap_friendly_query/mappings/table_mappings.py
@@ -0,0 +1,15 @@
+TABLE_MAPPINGS = {
+    "ekko": {
+        "purchase_order": "ebeln",
+        "company_code": "bukrs",
+        "document_type": "bsart",
+        "created_on": "aedat"
+    },
+    "ekpo": {
+        "purchase_order": "ebeln",
+        "item_number": "ebelp",
+        "material": "matnr",
+        "plant": "werks"
+    }
+    # add more table mappings
+}
